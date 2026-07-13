99060e24aa98a64946354ecd5b15b522714deba1
Cristhian <crsfa1204@gmail.com>
2026-07-13 16:56:46 -0500

    elminacion de sotre objetsd

--- app/hash_passwords.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const passwords = ['Admin123!', 'Tecnico123!', 'Practicante123!', 'Jefe123!', 'Usuario123!'];
-
-passwords.forEach(pass => {
-  const hash = bcrypt.hashSync(pass, 10);
-  console.log(`${pass} => ${hash}`);
-});
--- app/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const { isAuthenticated } = require('../middleware/auth');
-
-router.get('/', isAuthenticated, dashboardController.getDashboard);
-
-module.exports = router;
